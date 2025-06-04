0cc41a425b70aa01280a583171a15ca3262b08e6
James McKay <james.mckay@trausteknik.com>
2025-06-04 23:32:59 +1000

    Add some basic views

--- pjimcrm/urls.py
+++ pjimcrm/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("client/<int:client_id>/", views.client_detail, name="client_detail"),
+    path("client/<int:client_id>/project/<int:project_id>/", views.project_detail, name="project_detail"),
+    path("client/<int:client_id>/create-project", views.project_create, name="project_create"),
+    path("client/<int:client_id>/project/<int:project_id>/edit", views.project_edit, name="project_edit"),
+    path("client/<int:client_id>/invoice/<int:invoice_id>/", views.invoice_detail, name="invoice_detail"),
+    path("client/<int:client_id>/build-invoice", views.invoice_build, name="invoice_build"),
+    path("timer/", views.invoice_build, name="invoice_build"),
 ]
--- pjimcrm/views.py
+++ pjimcrm/views.py
@@ -3,4 +3,34 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello World. This is the crm index.")
+    if request.user.is_authenticated:
+        return HttpResponse("Hello Authed World. This is the crm index.")
+    else:
+        return HttpResponse("Hello Non-Authed World. This is the crm index.")
+
+
+def client_detail(request, client_id):
+    return HttpResponse("Hello World. Client id:" + client_id)
+
+
+def project_detail(request, client_id, project_id):
+    return HttpResponse("Hello World. Project id:" + project_id)
+
+def project_create(request, client_id, project_id):
+    return HttpResponse("Hello World. Project id:" + project_id)
+
+def project_edit(request, client_id, project_id):
+    return HttpResponse("Hello World. Project id:" + project_id)
+
+
+def invoice_detail(request, invoice_id):
+    return HttpResponse("Hello World. Invoice id:" + invoice_id)
+
+
+def invoice_build(request, client_id):
+    return HttpResponse("Build invoice. Client id:" + client_id)
+
+
+def timer_index(request):
+    return HttpResponse("Hello World. Timesheet index.")
+
