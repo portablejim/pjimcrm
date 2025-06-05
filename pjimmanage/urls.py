eacdd204842b7deab1d87302efd5c72600143269
James McKay <james.mckay@trausteknik.com>
2025-06-06 00:04:36 +1000

    Add default auth urls

--- pjimmanage/urls.py
+++ pjimmanage/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('crm/', include("pjimcrm.urls"))
+    path("accounts/", include("django.contrib.auth.urls")),
+    path('crm/', include("pjimcrm.urls")),
 ]
