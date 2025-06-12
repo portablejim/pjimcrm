7c0367bc389f6d065e697eb0c6f8faf58fe986e5
James McKay <james.mckay@trausteknik.com>
2025-06-12 12:40:43 +1000

    Add auth requirement to views

--- pjimcrm/views.py
+++ pjimcrm/views.py
@@ -12,31 +12,38 @@ def index(request):
     return render(request, "pjimcrm/home.html", {"client_list": client_list})
 
 
+@login_required()
 def client_detail(request, client_id):
     client_record = get_object_or_404(Client, pk=client_id)
     project_list = client_record.project_set.all()
     return render(request, "pjimcrm/client_detail.html", {"client_record": client_record, "project_list": project_list})
 
 
+@login_required()
 def project_detail(request, client_id, project_id):
     project_record = get_object_or_404(Project, pk=project_id)
     return render(request, "pjimcrm/project_detail.html", {"project_record": project_record})
 
+@login_required()
 def project_create(request, client_id, project_id):
     return HttpResponse("Hello World. Project id:" + str(project_id))
 
+@login_required()
 def project_edit(request, client_id, project_id):
     return HttpResponse("Hello World. Project id:" + str(project_id))
 
 
+@login_required()
 def invoice_detail(request, invoice_id):
     return HttpResponse("Hello World. Invoice id:" + str(invoice_id))
 
 
+@login_required()
 def invoice_build(request, client_id):
     return HttpResponse("Build invoice. Client id:" + str(client_id))
 
 
+@login_required()
 def timer_index(request):
     return HttpResponse("Hello World. Timesheet index.")
 
