63f6b53aed7bf9efb93b57af3433b8c05697699e
James McKay <james.mckay@trausteknik.com>
2025-06-12 13:05:47 +1000

    Make admin UI a bit better

--- pjimcrm/admin.py
+++ pjimcrm/admin.py
@@ -7,8 +7,31 @@ from .models import Invoice
 from .models import InvoiceLine
 from .models import TimesheetEntry
 
-admin.site.register(Client)
-admin.site.register(Project)
-admin.site.register(Invoice)
+class TimesheetEntryInline(admin.StackedInline):
+    model = TimesheetEntry
+    fields = ["target_user", "project", "description", "description_set", "length_raw", "timestamp_started"]
+    extra = 1
+    
+
+class ProjectAdmin(admin.ModelAdmin):
+    inlines = [TimesheetEntryInline]
+
+class ProjectInline(admin.TabularInline):
+    model = Project
+    extra = 1
+
+class ClientAdmin(admin.ModelAdmin):
+    inlines = [ProjectInline]
+
+class InvoiceLineInline(admin.TabularInline):
+    model = InvoiceLine
+    extra = 3
+
+class InvoiceAdmin(admin.ModelAdmin):
+    inlines = [InvoiceLineInline]
+
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Invoice, InvoiceAdmin)
 admin.site.register(InvoiceLine)
 admin.site.register(TimesheetEntry)
