c67d45a78a3ecc8f78d90956d14b21ca99d0b4a1
James McKay <james.mckay@trausteknik.com>
2025-06-19 18:17:08 +1000

    Add favicon and manifest

--- pjimmanage/urls.py
+++ pjimmanage/urls.py
@@ -15,9 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path, re_path
+from django.views.generic.base import RedirectView
+
+favicon_view = RedirectView.as_view(url='/static/favicon.ico', permanent=True)
 
 urlpatterns = [
+    re_path(r'^favicon\.ico$', favicon_view),
     path('admin/', admin.site.urls),
     path("accounts/", include("django.contrib.auth.urls")),
     path('crm/', include("pjimcrm.urls")),
