9b706a46a9771bec449d9c942b2a39f3c750d267
James McKay <james.mckay@trausteknik.com>
2025-06-20 23:24:13 +1000

    STATIC_ROOT has an underscore

--- pjimmanage/settings.py
+++ pjimmanage/settings.py
@@ -115,7 +115,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATICROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'static'
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS = [BASE_DIR / 'resources' / 'public']
